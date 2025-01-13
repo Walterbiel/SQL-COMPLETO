5bf17289022c435b452ee76d02eb966c8b5bc61c
walter <walterbiel1237@gmail.com>
2025-01-13 18:42:30 +0000

    ipdate filtros com where

--- Filtros com where.sql
+++ Filtros com where.sql
@@ -1,11 +1,15 @@
 -- Filtros básicos na tabela de clientes
 
--- Apenas clientes que são do Brasil ou Mexico
+-- 1. Apenas clientes que são do Brasil ou Mexico
 SELECT * 
 FROM customers
 WHERE COUNTRY = 'Brazil' OR COUNTRY = 'Mexico' 
+-- ou
+SELECT * 
+FROM customers
+WHERE COUNTRY in ('Brazil','Mexico')
 
--- Vendas em determinada região que iniciam com o mesmo número de código postal (8)
+-- 2.Vendas em determinada região que iniciam com o mesmo número de código postal (8)
 SELECT 
 order_id,
 order_date,
@@ -15,6 +19,20 @@ ship_postal_code,
 LENGTH(ship_postal_code) AS LEN
 FROM orders
 WHERE ship_postal_code LIKE '8%' AND LENGTH(ship_postal_code) > 4
+
+-- 3.Fretes entre 10 reais e 25 reais
+SELECT 
+*
+FROM orders
+WHERE freight BETWEEN 10 AND 25
+
+--4.Fretes que não estão entre 0 e 10 reais
+
+SELECT 
+*
+FROM orders
+WHERE freight NOT BETWEEN 0 AND 10
+
 
 
 
